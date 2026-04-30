d55fa1655c3cc027e6437a56ca27e6a936949828
muhduvais <uvaisp110@gmail.com>
2026-04-30 15:16:50 +0530

    feat: implement user registration with password hashing

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private usersService: UsersService) {}
+
+    async register(dto: RegisterDto) {
+        const { name, email, password, role } = dto;
+
+        const existingUser = await this.usersService.findByEmail(email);
+        if (existingUser) {
+            throw new BadRequestException('User already exists');
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await this.usersService.create({
+            name,
+            email,
+            password: hashedPassword,
+            role
+        });
+
+        return {
+            message: 'User registered successfully',
+            user
+        }
+    }
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectModel(User.name) private userModel: Model<UserDocument>,
+    ) {}
+
+    async create(data: Partial<User>) {
+        return this.userModel.create(data);
+    }
+
+    async findByEmail(email: string) {
+        return this.userModel.findOne({ email });
+    }
+}
