bf8e4d5ec333b5ba80b2ebd9bbad83f2e6e5c4f4
muhduvais <uvaisp110@gmail.com>
2026-04-30 12:47:51 +0530

    feat: start database design with user schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './config/database.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
+    UsersModule,
   ],
 })
 export class AppModule {}
